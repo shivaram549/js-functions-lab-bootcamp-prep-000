4772cf3ac928f83c2251155a4f2d34d72025d1b2
shivaram549 <shivarampandramish549@gmail.com>
2017-09-01 14:42:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // write your code below!
+function happyHolidays()
+{
+  return "Happy holidays!"
+}
+function happyHolidaysTo(String)
+{
+  //var name = "shiva"
+  return "Happy holidays, ${name}!"
+}
+function happyHolidayTo(String,String)
+{
+  return "Happy ${holiday}, ${name}!"
+}
+function holidayCountdown(int,String)
+{
+  return "It's ${days} days until ${holiday}!"
+}
